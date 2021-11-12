3b050f4b4fc93116b54970194eb4c6e0c1e58726
Ал <alexandertimoshenko1988@mail.ru>
2021-11-12 22:40:34 +0300

    Lesson 3 HW 1.1.

---
+++ src/main/java/Lessonthree/HWSecondExc.java
@@ -0,0 +1,12 @@
+package Lessonthree;
+
+public class HWSecondExc {
+    public static void main(String[] args) {
+        int n = 7;
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        System.out.print(result);
+    }
+}
