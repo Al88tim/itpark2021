015b5daed7693fc54271248c81fc3cd757fa142c
Ал <alexandertimoshenko1988@mail.ru>
2021-11-17 13:28:08 +0300

    HWFour

--- src/main/java/LessonFour/HWFour.java
+++ src/main/java/LessonFour/HWFour.java
@@ -3,19 +3,21 @@ package LessonFour;
 public class HWFour {
     public static void main(String[] args) {
         System.out.print(findSquareRoot(2));
-                /*
+        /*
          *Class to find square root of number
          */
     }
+
     private static double findSquareRoot(int value) {
         double num;
         double half = (double) value / 2;
         do {
             num = half;
-            half = (num + (value / num)) / 2;}
+            half = (num + (value / num)) / 2;
+        }
 
-            while ((num - half) != 0) ;
-            return half;
+        while ((num - half) != 0);
+        return half;
     }
 }
 
