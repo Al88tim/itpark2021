6478e383557c52298cb826a0c67aaf8016b185f6
Ал <alexandertimoshenko1988@mail.ru>
2021-11-20 21:02:49 +0300

    HWFive

---
+++ src/main/java/LessonFive/HWFive.java
@@ -0,0 +1,18 @@
+package LessonFive;
+
+public class HWFive<previousNum> {
+    public static void main(String[] args) {
+
+        int n0 = 0;
+        int n1 = 1;
+        int n2;
+        System.out.print(n0 + " " + n1 + " ");
+        for (int i = 3; i <= 18; i++) {
+            n2 = n0 + n1;
+            System.out.print(n2 + " ");
+            n0 = n1;
+            n1 = n2;
+        }
+        System.out.println();
+    }
+}
