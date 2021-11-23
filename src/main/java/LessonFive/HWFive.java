c2c61f9214d0185887f4cfc99233dc639d5fb467
Ал <alexandertimoshenko1988@mail.ru>
2021-11-23 16:03:56 +0300

    HWSix

--- src/main/java/LessonFive/HWFive.java
+++ src/main/java/LessonFive/HWFive.java
@@ -8,7 +8,7 @@ public class HWFive<previousNum> {
         int n2;
         System.out.print(n0 + " " + n1 + " ");
         for (int i = 3; i <= 18; i++) {
-            n2 = n0 + n1;
+            n2 = n0 + n1 ;
             System.out.print(n2 + " ");
             n0 = n1;
             n1 = n2;
